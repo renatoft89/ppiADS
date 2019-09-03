2d5bae3d4f81d70ede97f45810c7698f4baa8979
Renato <renatoft89@gmail.com>
2019-09-02 19:41:48 -0500

    1.0.0

---
+++ venvFaao/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\renato\onedrive - academico - secretaria do estado da educação de são paulo\django projetos\venvmt\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
