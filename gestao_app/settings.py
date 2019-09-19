c7270c8a01ad25729b975c7e73e580e2c28b572e
Matheus <matheusferreiragold@gmail.com>
2019-09-19 00:04:04 -0500

    2.0.8

--- gestao_app/settings.py
+++ gestao_app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import djago-heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,4 +140,3 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-django_heroku.settings(locals())
